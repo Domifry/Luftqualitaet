9c1483f55259dcc5137a36f67ea75f027bd41be2
Dominic <lindner.dominic@gmail.com>
2021-09-29 02:19:12 +0200

    Update fehler.py

--- fehler.py
+++ fehler.py
@@ -1,13 +1,11 @@
-
 #!/usr/bin/env python3
 
-import httplib, urllib
-
-conn = httplib.HTTPSConnection("api.pushover.net:443")
+import http.client, urllib
+conn = http.client.HTTPSConnection("api.pushover.net:443")
 conn.request("POST", "/1/messages.json",
-  urllib.urlencode({
-    "token": "TOKEN",
-    "user": "USER",
+  urllib.parse.urlencode({
+    "token": "Token",
+    "user": "usertoken",
     "message": "Das Script hat sich neu gestartet!",
   }), { "Content-type": "application/x-www-form-urlencoded" })
 conn.getresponse()
